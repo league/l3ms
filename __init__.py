4c34aecfd589a39a3db1e62543be0be23a11e794
Christopher League <league@contrapunctus.net>
2010-12-22 17:21:11 -0500

    Dumb down django password storage for compat with Apache auth.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# Module initialization for djlms project.
+
+from hashlib import md5
+from django.contrib.admin.models import User
+
+# Monkey-patch django User class to dumb down the password storage for
+# compatibility with pgsql Apache auth module, which understands only
+# unsalted MD5, crypt (yuk), or base64 (not one-way).
+
+def set_bland_md5_password(user, raw_password):
+    user.password = md5(raw_password).hexdigest()
+
+def check_bland_md5_password(user, raw_password):
+    return user.password == md5(raw_password).hexdigest()
+
+User.set_password = set_bland_md5_password
+User.check_password = check_bland_md5_password
