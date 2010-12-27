d5f979eba4509b5fa101658e127319784195c165
Christopher League <league@contrapunctus.net>
2010-12-26 23:20:41 -0500

    login, logout, and profile pages.

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+@login_required
+def profile(request):
+    return HttpResponse('Hello, '+ request.user.username)
+
--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
+from django.conf import global_settings as default
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from djlms.settings import rel_url
 import django.contrib.auth.views
+import djlms.accounts.views
 import djlms.misc.views
 
 admin.autodiscover()
@@ -9,6 +11,8 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     (rel_url('misc/$'), djlms.misc.views.hello),
-    (rel_url('accounts/login/$'), django.contrib.auth.views.login),
+    (rel_url(default.LOGIN_URL[1:]), django.contrib.auth.views.login),
+    (rel_url(default.LOGOUT_URL[1:]), django.contrib.auth.views.logout),
+    (rel_url(default.LOGIN_REDIRECT_URL[1:]), djlms.accounts.views.profile),
     (rel_url(r'admin/'), include(admin.site.urls)),
 )
