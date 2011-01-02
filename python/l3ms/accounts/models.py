f681afad9c19bd1861e40dc0289cda79709ed6c3
Christopher League <league@contrapunctus.net>
2011-01-02 13:12:42 -0500

    remove obsolete activation keys from accounts app

--- python/l3ms/accounts/models.py
+++ python/l3ms/accounts/models.py
@@ -28,42 +28,4 @@ class UserProfile(models.Model):
                     self.user.last_name and self.user.email and
                     self.user.has_usable_password())
 
-RESET_PASSWORD = 'R'
-VALIDATE_EMAIL = 'E'
-INVITE_USER = 'I'
-
-class ActivationManager(models.Manager):
-    def create(self, user, action, email):
-        r = email + random().hex()
-        k = urlsafe_b64encode(md5(r).digest()[1:])
-        ak = self.model(key=k, action=action, user=user, email=email)
-        # send email here too?
-        ak.save()
-        return ak
-
-class ActivationKey(models.Model):
-    key = models.CharField(max_length=20, unique=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    action = models.CharField(max_length=1, choices=(
-            (RESET_PASSWORD, 'Reset password'),
-            (VALIDATE_EMAIL, 'Validate email'),
-            (INVITE_USER, 'Invite user')
-            ))
-    user = models.ForeignKey(User)
-    email = models.EmailField()
-
-    objects = ActivationManager()
-
-    def __unicode__(self):
-        return '%s %s %s' % (
-            self.get_action_display(),
-            self.email,
-            self.key
-            )
-
-    @models.permalink
-    def get_absolute_url(self):
-        return ('activate', (), {'key': self.key})
-
 admin.site.register(UserProfile)
-admin.site.register(ActivationKey)
