04ce9ac17e79671fc4d21f325e81794a10617db2
Christopher League <league@contrapunctus.net>
2011-01-06 12:18:08 -0500

    all_courses page is satisfactory.

--- python/l3ms/courses/views.py
+++ python/l3ms/courses/views.py
@@ -1,7 +1,15 @@
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from models import Course
 
 def all_courses(request):
-    return HttpResponse('not implemented')
+    my_courses = Course.objects.filter(enrollment__user=request.user)
+    return render_to_response('courses/all.html',
+                              {'user': request.user,
+                               'site_name': settings.SITE_NAME,
+                               'courses': Course.objects.all(),
+                               'my_courses': my_courses})
 
 def one_course(request, tag):
     return HttpResponse('%s not implemented' % tag)
